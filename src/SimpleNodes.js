c194580e826d969d28400a3433854546667871f9
Michael McKenzie <mckenzma@gmail.com>
2021-11-26 14:39:31 -0500

    Add second team nodes

--- src/SimpleNodes.js
+++ src/SimpleNodes.js
@@ -9,7 +9,6 @@ import {
   Circle,
   Line /*, Node*/
 } from 'react-konva';
-// import { Solarize } from 'konva/types/filters/Solarize';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,72 +32,119 @@ export default function SimpleNodes() {
   const pitchLength = 120; // valid range 100-130
   const pitchWidth = 80; // valid range 50-100
 
-  const [nodes, setNodes] = useState([
+  const [nodesHome, setNodesHome] = useState([
     {
       id: 'n1',
-      x: pitchLength*3,
+      x: pitchLength*3-25,
       y: pitchWidth/2*3,
-      //r: 10,
     },
     {
       id: 'n2',
       x: (pitchLength-18)*3,
       y: 1*pitchWidth/5*3,
-      //r: 10
     },
     {
       id: 'n3',
       x: (pitchLength-18)*3,
       y: 2*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n4',
       x: (pitchLength-18)*3,
       y: 3*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n5',
       x: (pitchLength-18)*3,
       y: 4*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n6',
       x: (pitchLength/2)*3,
       y: 1*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n7',
       x: (pitchLength/2)*3,
       y: 2*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n8',
       x: (pitchLength/2)*3,
       y: 3*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n9',
       x: (pitchLength/2)*3,
       y: 4*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n10',
       x: 18*3,
       y: 2*pitchWidth/5*3,
-      //r: 10,
     },
     {
       id: 'n11',
       x: 18*3,
       y: 3*pitchWidth/5*3,
-      //r: 10
+    }
+  ]);
+
+  const [nodesAway, setNodesAway] = useState([
+    {
+      id: 'n1',
+      x: 25,
+      y: pitchWidth/2*3,
+    },
+    {
+      id: 'n2',
+      x: 18*3,
+      y: 1*pitchWidth/5*3,
+    },
+    {
+      id: 'n3',
+      x: 18*3,
+      y: 2*pitchWidth/5*3,
+    },
+    {
+      id: 'n4',
+      x: 18*3,
+      y: 3*pitchWidth/5*3,
+    },
+    {
+      id: 'n5',
+      x: 18*3,
+      y: 4*pitchWidth/5*3,
+    },
+    {
+      id: 'n6',
+      x: (pitchLength/2)*3,
+      y: 1*pitchWidth/5*3,
+    },
+    {
+      id: 'n7',
+      x: (pitchLength/2)*3,
+      y: 2*pitchWidth/5*3,
+    },
+    {
+      id: 'n8',
+      x: (pitchLength/2)*3,
+      y: 3*pitchWidth/5*3,
+    },
+    {
+      id: 'n9',
+      x: (pitchLength/2)*3,
+      y: 4*pitchWidth/5*3,
+    },
+    {
+      id: 'n10',
+      x: (pitchLength-18)*3,
+      y: 2*pitchWidth/5*3,
+    },
+    {
+      id: 'n11',
+      x: (pitchLength-18)*3,
+      y: 3*pitchWidth/5*3,
     }
   ]);
 
@@ -112,44 +158,47 @@ export default function SimpleNodes() {
 
               {/* width/height here sets the overall size of the canvas. need to integrate with size of football pitch so they scale appropriately */}
               <Stage width={pitchLength*3} height={pitchWidth*3}>
+                
+                {/* Field markings layer */}
                 <Layer>
                   {/* Touchline */}
                   <Line
                     points={[0, 0, pitchLength*3, 0, pitchLength*3, pitchWidth*3,0,pitchWidth*3,0,0]}
-                    // closed
-                    stroke="black"
+                    closed
+                    stroke="white"
+                    fill="green"
                   />
                   {/* Mid field */}
                   <Line
                     points={[(pitchLength/2)*3, 0, (pitchLength/2)*3, pitchWidth*3]}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Center Cirlce */}
                   <Circle 
                     x={(pitchLength/2)*3}
                     y={pitchWidth/2*3}
                     radius={10*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Center circle spot */}
                   <Circle 
                     x={(pitchLength/2)*3}
                     y={pitchWidth/2*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Penalty arc (left) */}
                   <Circle 
                     x={12*3}
                     y={pitchWidth/2*3}
                     radius={10*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Penalty box (left) */}
                   <Line 
                     points={[0,(pitchWidth/2-18)*3,18*3,(pitchWidth/2-18)*3,18*3,(pitchWidth/2+18)*3,0,(pitchWidth/2+18)*3]}
-                    stroke="black"
-                    fill="white"
+                    stroke="white"
+                    fill="green"
                     closed
                   />
                   {/* Penalty circle spot (left) */}
@@ -157,13 +206,12 @@ export default function SimpleNodes() {
                     x={12*3}
                     y={pitchWidth/2*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* 6-yd box (left) */}
                   <Line 
                     points={[0,(pitchWidth/2-10)*3,6*3,(pitchWidth/2-10)*3,6*3,(pitchWidth/2+10)*3,0,(pitchWidth/2+10)*3]}
-                    stroke="black"
-                    fill="white"
+                    stroke="white"
                     closed
                   />
                   {/* Penalty arc (right) */}
@@ -171,13 +219,13 @@ export default function SimpleNodes() {
                     x={(pitchLength-12)*3}
                     y={pitchWidth/2*3}
                     radius={10*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Penalty box (right) */}
                   <Line 
                     points={[(pitchLength-18)*3,(pitchWidth/2-18)*3,pitchLength*3,(pitchWidth/2-18)*3,pitchLength*3,(pitchWidth/2+18)*3,(pitchLength-18)*3,(pitchWidth/2+18)*3]}
-                    stroke="black"
-                    fill="white"
+                    stroke="white"
+                    fill="green"
                     closed
                   />
                   {/* Penalty circle spot (right) */}
@@ -185,13 +233,12 @@ export default function SimpleNodes() {
                     x={(pitchLength-12)*3}
                     y={pitchWidth/2*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* 6-yd box (right) */}
                   <Line 
                     points={[(pitchLength-6)*3,(pitchWidth/2-10)*3,pitchLength*3,(pitchWidth/2-10)*3,pitchLength*3,(pitchWidth/2+10)*3,(pitchLength-6)*3,(pitchWidth/2+10)*3]}
-                    stroke="black"
-                    fill="white"
+                    stroke="white"
                     closed
                   />
                   {/* Corner arc (top left) */}
@@ -199,81 +246,101 @@ export default function SimpleNodes() {
                     x={0}
                     y={0}
                     radius={1*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Corner arc (top right) */}
                   <Circle 
                     x={pitchLength*3}
                     y={0}
                     radius={1*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Corner arc (bottom left) */}
                   <Circle 
                     x={0}
                     y={pitchWidth*3}
                     radius={1*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Corner arc (bottom right) */}
                   <Circle 
                     x={pitchLength*3}
                     y={pitchWidth*3}
                     radius={1*3}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Goal post left-top */}
                   <Circle 
                     x={0}
                     y={(pitchWidth/2-4)*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Goal post left-bot */}
                   <Circle 
                     x={0}
                     y={(pitchWidth/2+4)*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Goal post right-top */}
                   <Circle 
                     x={pitchLength*3}
                     y={(pitchWidth/2-4)*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
                   />
                   {/* Goal post right-bot */}
                   <Circle 
                     x={pitchLength*3}
                     y={(pitchWidth/2+4)*3}
                     radius={1}
-                    stroke="black"
+                    stroke="white"
+                  />
+                </Layer>
+
+                <Layer>
+                  {/* Ball */}
+                  <Circle 
+                    x={pitchLength/2*3}
+                    radius={5}
+                    y={pitchWidth/2*3}
+                    stroke="yellow"
+                    fill="yellow"
+                    draggable
                   />
                 </Layer>
 
                 {/* Players Layer */}
                 <Layer>
-                  {nodes.map(n => {
+                  {nodesHome.map(n => {
                     return (
                       <Circle
-                        x={n.x}
+                        x={n.x+15}
                         y={n.y}
                         radius={10}
                         key={n.id}
                         opacity={n.opacity}
-                        fill="green"
+                        fill="red"
+                        stroke="black"
                         draggable
-                        // onMouseEnter={e => {
-                        //   // increaseOpacity(e, n.id, nodes, '1')
-                        //   // increaseOpacityOther(e, n.linked2, nodes2, '2')
-                        // }
-                        // }
-                        // onMouseLeave={e => {
-                        //   // decreaseOpacity(e, nodes, '1')
-                        //   // decreaseOpacity(e, nodes2, '2')
-                        // }
-                        // }
+                      />
+                    );
+                  })}
+                </Layer>
+
+                <Layer>
+                  {nodesAway.map(n => {
+                    return (
+                      <Circle
+                        x={n.x-15}
+                        y={n.y}
+                        radius={10}
+                        key={n.id}
+                        opacity={n.opacity}
+                        fill="blue"
+                        stroke="black"
+                        draggable
                       />
                     );
                   })}
